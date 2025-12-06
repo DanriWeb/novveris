048695998448f6ab1b0a27856dea425e91120731
DanriWeb <danri.web@gmail.com>
2025-12-06 18:30:46 +0500

    feat: добавить ассеты в shared слой
    Добавлены базовые ассеты проекта в "shared/assets":
    - Шрифты: Arturito, Coolvetica, Inter
    - Иконки: логотип, социальные сети
    - 9 SVG иллюстраций

---
+++ apps/frontend/src/shared/assets/icons/index.ts
@@ -0,0 +1,21 @@
+// Other icons
+import logoIcon from "./other/logo-icon.svg";
+
+// Social icons
+import discordIcon from "./socials/discord.svg";
+import googleIcon from "./socials/google.svg";
+import instagramIcon from "./socials/instagram.svg";
+import telegramIcon from "./socials/telegram.svg";
+import vkIcon from "./socials/vk.svg";
+
+export const icons = {
+  // Other icons
+  logoIcon,
+
+  // Social icons
+  discordIcon,
+  googleIcon,
+  instagramIcon,
+  telegramIcon,
+  vkIcon,
+};
