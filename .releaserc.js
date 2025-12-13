d37796c490b630aa5b4559122403870781981cd2
DanriWeb <danri.web@gmail.com>
2025-12-14 01:14:50 +0500

    fix(config): исправить отображение эмодзи в CHANGELOG

--- .releaserc.js
+++ .releaserc.js
@@ -7,6 +7,8 @@ module.exports = {
         preset: "conventionalcommits",
       },
     ],
+
+    // Единственный источник кастомизации контента
     [
       "@semantic-release/release-notes-generator",
       {
@@ -28,41 +30,33 @@ module.exports = {
         writerOpts: {
           transform: (commit) => {
             if (!commit.scope) {
-              return { ...commit, scope: "general" };
+              commit.scope = "general";
             }
             return commit;
           },
-          commitPartial: `* {{#if scope}}**{{scope}}:** {{/if}}{{subject}} ([{{hash}}]({{@root.host}}/{{@root.owner}}/{{@root.repository}}/commit/{{hash}}))`,
+          commitPartial:
+            "* {{#if scope}}**{{scope}}:** {{/if}}{{subject}} ([{{hash}}]({{@root.host}}/{{@root.owner}}/{{@root.repository}}/commit/{{hash}}))",
         },
       },
     ],
+
+    // Просто пишет release notes в файл, без кастомизации
     [
       "@semantic-release/changelog",
       {
-        preset: "conventionalcommits",
-        presetConfig: {
-          types: [
-            { type: "feat", section: "🚀 Features" },
-            { type: "fix", section: "🔧 Fixes" },
-            { type: "docs", section: "📝 Docs" },
-            { type: "style", section: "💎 Styles" },
-            { type: "refactor", section: "♻️ Refactor" },
-            { type: "perf", section: "⚡ Performance" },
-            { type: "test", section: "✅ Tests" },
-            { type: "build", section: "🏗️ Build" },
-            { type: "ci", section: "👷 CI" },
-            { type: "chore", section: "🛠️ Chore" },
-          ],
-        },
+        changelogFile: "CHANGELOG.md",
       },
     ],
+
     "@semantic-release/npm",
+
     [
       "@semantic-release/git",
       {
-        message: "chore(release): 🎉 v${nextRelease.version} version!",
+        message: "chore(release): 🎉 v${nextRelease.version}",
       },
     ],
+
     "@semantic-release/github",
   ],
 };
